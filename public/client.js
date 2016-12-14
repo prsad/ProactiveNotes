98b41924a99775b9d052e00cdb2fc8d859c811a3
Kshitiz Saxena <kshitiz.saxena@oracle.com>
2016-12-14 22:54:10 +0530

    added comments

--- public/client.js
+++ public/client.js
@@ -1,17 +1,19 @@
+//define required dependencies
 var duplexEmitter = require('duplex-emitter');
 var reconnect = require('reconnect');
 
+//get hold of html "result" element
+//it will be used to push data to html page
 var result = document.getElementById('result');
 
+//create a reconnectine stream
 var stream = reconnect(function(stream) {
   console.log('connected');
- 
-  var server = duplexEmitter(stream);
- 
-  /*var interval = setInterval(function() {
-    server.emit('ping', Date.now());
-  }, 1000);*/
 
+  //create a duplex event emitter to server
+  var server = duplexEmitter(stream);
+
+  //get hold of actor name passed in query string and pass it down to server
   var actorName =  getParameterByName('actor');
   if(!actorName){
     console.log("Actor name not found in query string");
@@ -19,6 +21,7 @@ var stream = reconnect(function(stream) {
   }
   server.emit('actor', actorName);
 
+  //whenever a new AI is notified, display it in html page
   server.on('newAI', function(data) {
     console.log('New AI from server : ' + data);
     var obj = JSON.parse(data);
@@ -32,12 +35,9 @@ var stream = reconnect(function(stream) {
     details =  details + "------------------------------------\n";
     result.appendChild(document.createTextNode(details));
   });
- 
-  stream.once('end', function() {
-    clearInterval(interval);
-  });
 }).connect('/websocket');
 
+//function to retrieve query param from url
 function getParameterByName(name, url) {
   if (!url) {
     url = window.location.href;
